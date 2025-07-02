90cbc686a7e9d20e988f8a4b775efa2dc3b0c02b
Harish Garg <rishusharma123321@gmail.com>
2025-07-02 10:36:47 +0530

    second method

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
 import { dataSourceOptions } from './config/data-source';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
-  imports: [UploadModule, TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [UploadModule, TypeOrmModule.forRoot(dataSourceOptions),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload/files'), 
+      serveRoot:`/upload/files`
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-
-
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.useStaticAssets(join(__dirname, '..', 'upload/files'), {
-    prefix: '/upload/files/',
-  });
+  const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT ?? 3000);
 }
